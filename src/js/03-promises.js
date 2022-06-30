3547ef0ca3ac8f9b9e0989f42a46d83e976f895b
Andrei <andrei.lerke@gmail.com>
2022-06-30 17:36:49 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,18 +1,47 @@
 import Notiflix from 'notiflix';
 
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
+const refs = {
+  firstDelayRef: document.querySelector('input[name="delay"]'),
+  delayStepRef: document.querySelector('input[name="step"]'),
+  amountRef: document.querySelector('input[name="amount"]'),
+  submitBtn: document.querySelector('button[type="submit"]'),
+};
 
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+let delay = 0;
+
+refs.submitBtn.addEventListener('click', onClickSubmitBtn);
+
+function onClickSubmitBtn(event) {
+  event.preventDefault();
+
+  let step = Number(refs.delayStepRef.value);
+  let amount = Number(refs.amountRef.value);
+
+  delay = Number(refs.firstDelayRef.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    delay += step;
+
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
